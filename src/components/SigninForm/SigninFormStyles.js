ba2edb9b6c9e5eed3ce0039316e8afbe11ccfefb
Emmanuel Eze <emmanueleze.official@gmail.com>
2022-08-26 11:25:16 +0100

    chore: cleaned unsused styles

--- src/components/SigninForm/SigninFormStyles.js
+++ src/components/SigninForm/SigninFormStyles.js
@@ -11,11 +11,6 @@ export const $Container = styled.div`
   z-index: 0;
   overflow: hidden;
   background: #0285a3;
-  /* background: linear-gradient(
-    108deg,
-    rgba(1, 147, 86, 1) 0%,
-    rgba(10, 201, 122, 1) 100%
-  ); */
 `;
 
 export const $FormWrap = styled.div`
