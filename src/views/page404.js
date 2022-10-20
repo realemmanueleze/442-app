f9bf5427bd0e429f28af467fdbb272a04ed55fa0
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-10-20 20:45:08 +0100

    feat: hide nav on page 404

--- src/views/page404.js
+++ src/views/page404.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { NavContext } from 'components/NavContext';
 
 function Page404() {
-  return <div>404 - Page not found</div>;
+  const { setNavIsOpen } = useContext(NavContext);
+  useEffect(() => {
+    setNavIsOpen(false);
+  });
+  const styles = {
+    margin: '20px',
+  };
+  return <div style={styles}>404 - Page not found</div>;
 }
 
 export default Page404;
