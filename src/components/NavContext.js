393b830b1c5c8405a6c494c468c9b7d8a054128f
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-10-20 19:30:37 +0100

    chore: cleaned up console logs in NavContext

--- src/components/NavContext.js
+++ src/components/NavContext.js
@@ -2,12 +2,9 @@ import React, { useState, createContext } from 'react';
 import PropTypes from 'prop-types';
 
 export const NavContext = createContext();
-console.log(window.location.pathname);
 
 function NavContextProvider({ children }) {
   const [navIsOpen, setNavIsOpen] = useState(true);
-  const windowLocation = window.location.href;
-  console.log(windowLocation);
 
   const toggleNavIsOpen = () => {
     setNavIsOpen((prev) => !prev);
